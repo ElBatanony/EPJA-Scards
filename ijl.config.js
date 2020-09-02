aaa97b915dd687940aa836450765e0eaa4e05d87
ElBatanony <ahmedelbatanony@hotmail.com>
2020-09-02 18:44:45 +0300

    Add vue-loader

--- ijl.config.js
+++ ijl.config.js
@@ -2,9 +2,12 @@ const pkg = require("./package.json");
 
 let pkgName = pkg.name.replace('uds-', '')
 
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
 module.exports = {
   apiPath: "stubs/api",
   webpackConfig: {
+    mode: 'development',
     output: {
       publicPath: `/static/${pkgName}/${pkg.version}/`
     },
@@ -12,7 +15,28 @@ module.exports = {
       alias: {
         'vue$': 'vue/dist/vue.esm.js'
       }
-    }
+    },
+    module: {
+      rules: [
+        { parser: { system: false } },
+        {
+          test: /\.tsx?$/,
+          loader: "awesome-typescript-loader"
+        },
+        {
+          test: /\.(jpe?g|gif|png|svg|woff|ttf|eot|wav|mp3)$/,
+          loader: "file-loader"
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        }
+      ]
+    },
+    plugins: [
+
+      new VueLoaderPlugin()
+    ]
   },
   config: {
     'main.api.base.url': '/api',
