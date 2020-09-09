39130f3461cd14374861c06bd74ec81e23bd250c
ElBatanony <ahmedelbatanony@hotmail.com>
2020-09-09 18:27:10 +0300

    Return Access cross-origin header

--- stubs/api/index.js
+++ stubs/api/index.js
@@ -1,14 +1,14 @@
 const router = require("express").Router();
 const scardsRoute = require("./scards");
 
-// router.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "http://89.223.91.151:8080");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
+router.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://89.223.91.151:8080");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 router.get("/testData", (req, res) => {
   res.send({ status: 200, msg: "Test API Ok" });
