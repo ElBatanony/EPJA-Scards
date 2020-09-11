75c8ffac06bc0d3b8d714f937da0da89e6ec0163
ElBatanony <ahmedelbatanony@hotmail.com>
2020-09-11 14:46:54 +0300

    Add workflow setup (English session)

--- stubs/api/index.js
+++ stubs/api/index.js
@@ -1,5 +1,10 @@
 const router = require("express").Router();
 const scardsRoute = require("./scards");
+const workflow = require("./workflow/workflow.json");
+
+const delayAnswer = (req, res, next) => {
+  setTimeout(next, 300);
+};
 
 router.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://89.223.91.151:8080");
@@ -12,6 +17,27 @@ router.use(function (req, res, next) {
 
 router.get("/testData", (req, res) => {
   res.send({ status: 200, msg: "Test API Ok" });
+});
+
+router.get("/api/workflow", (req, res) => {
+  const { cmd, name, data } = req.query;
+  console.log(cmd, name, data);
+  let flowName = name,
+    stateName;
+  if (cmd == "start") stateName = workflow.flows[flowName].init;
+  if (cmd == "event") {
+    flowName = req.session.workflow.flowName;
+    stateName =
+      workflow.flows[flowName].states[req.session.workflow.stateName].events[
+        name
+      ].newState;
+  }
+
+  req.session.workflow = {
+    flowName,
+    stateName,
+  };
+  res.send({ flowName, stateName });
 });
 
 router.use(scardsRoute);
--- stubs/api/scards.js
+++ stubs/api/scards.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
 
-const SCARDS_URL = "/scards";
+const SCARDS_URL = "/api/scards";
 let idCounter = 10;
 
 var scards;
